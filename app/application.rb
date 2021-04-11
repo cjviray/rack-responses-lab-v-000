f1227fd19f51deceb7a4c8b649438a3d33def54e
Christian V <viray.christianj@gmail.com>
2021-04-11 23:40:24 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Reponse.new
     if Time.now.hour < 12
-      resp.write "Good Monring!"
+      resp.write "Good Morning!"
     else 
       resp.write "Good Afternoon!"
     end 
